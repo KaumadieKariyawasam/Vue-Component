31781d190bae1641ba5b842ddc577ab594ebef2a
KAW <kkawmadie@gmail.com>
2020-10-27 09:55:54 +0530

    new project

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!--img src="./assets/logo.png"--->
+    <!--router-view/--->
+    {{title}}
+    <!--hild v-on:changeTitle="updateText($event)"/-->
+    <Login/>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login'
+export default {
+  name: 'App',
+  components:{
+    Login
+  },
+  data(){
+    return{
+      title:"Chils to parent"
+    }
+  },
+  methods:{
+    updateText(title){
+      this.title=title;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input type="text" placeholder="Enter User Name"/> <br><br>
+        <input type="password" placeholder="Enter pass"/> <br><br>
+        <button v-on:click="getLogin"> LogIn</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Login',
+    data(){
+        return{
+            loginForm:{
+                username:null,
+                password:null
+            }
+        }
+    },
+    methods:{
+        getLogin(){
+            console.log('login');
+        }
+    }
+}
+</script>
